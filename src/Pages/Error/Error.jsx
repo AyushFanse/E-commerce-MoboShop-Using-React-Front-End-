815bb43d9ae76f8b6be21fbd1f332a972b3a22ea
Ayush Bhagwan Fanse <87874012+AyushFanse@users.noreply.github.com>
2022-05-06 02:49:18 +0530

    E-Commerce Website

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
 import { Grid } from '@mui/material';
@@ -14,21 +13,4 @@ const Error = (props) => {
     );
 }
 
-=======
-import React from 'react';
-import Navbar from '../../Components/Navbar/Navbar';
-import { Grid } from '@mui/material';
-
-const Error = (props) => {
-    return (
-        <>
-            <Navbar page={'Error'}/>
-            <Grid container id='ErrorCont'>
-                <img src="https://i.ibb.co/SJdXspD/pngegg-2.png" alt="" />
-            </Grid>            
-        </>
-    );
-}
-
->>>>>>> 92347de19b2dffdddb9377a24bd634ef5d0f91a2
 export default Error;
